d9d8375cb2d2f874e85d61ad4bd6e4d150502795
Marta Fuks <fuks.marta@wp.pl>
2020-05-28 20:40:21 +0200

    new section: login: style

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from "./components/home/Home";
 import Shop from "./components/shop/Shop";
 import Contact from "./components/contact/Contact";
 import AboutUs from "./components/about_us/AboutUs";
+import Login from "./components/login/Login";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
               <Route exact path='/shop' render={() => <Shop path="/shop"/>}/>
               <Route exact path='/contact' render={() => <Contact path="/contact"/>}/>
               <Route exact path='/about_us' render={() => <AboutUs path="/about_us"/>}/>
+              <Route exact path='/login' render={() => <Login path="/login"/>}/>
           </>
       </HashRouter>
   );
--- src/components/general_components/header/logo/Logo.js
+++ src/components/general_components/header/logo/Logo.js
@@ -1,20 +1,14 @@
 import React, {Component} from 'react';
-import "./_log_register.scss"
+import "./_logo.scss"
 
-class LogRegister extends Component
+class Logo extends Component
 {
     render() {
         return (
             <>
-                <header className="lr">
-                    <div className="container lr-cont">
-                        <section className="lr-logo">
-
-                        </section>
-                        <section className="lr-user">
-                            <i className="far fa-user fa-2x lr-user-icon"/>
-                            <h4 className="lr-user-login">Login</h4>
-                        </section>
+                <header className="logo">
+                    <div className="container logo-cont">
+                        <section className="logo-content"/>
                     </div>
                 </header>
             </>
@@ -22,4 +16,4 @@ class LogRegister extends Component
     }
 }
 
-export default LogRegister;
+export default Logo;
