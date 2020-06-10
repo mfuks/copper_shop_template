6402e82acc0dc2abaf538c6556337672be452702
Marta Fuks <fuks.marta@wp.pl>
2020-06-10 12:11:37 +0200

    registration: bug fixed

--- src/components/general_components/header/log_register/LogRegister.js
+++ src/components/general_components/header/log_register/LogRegister.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import "./_log_register.scss"
 import {Link} from "react-router-dom";
-import Logo from "../logo/Logo";
 import LogoPlain from "../logo/logo_plain/LogoPlain";
 
 class LogRegister extends Component
